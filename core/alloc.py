56876aa93ddcf640af7b6741431e084a0990c96e
Martin Milata <martin@martinmilata.cz>
2020-10-23 12:57:54 +0200

    wip

--- core/alloc.py
+++ core/alloc.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 from types import SimpleNamespace
 import click
 
+HERE = Path(__file__).parent.resolve()
 
 def parse_alloc_data(alloc_data):
     parsed_data = {}
@@ -30,17 +32,23 @@ def cli(ctx, alloc_data, type):
     ctx.obj = SimpleNamespace(data=parse_alloc_data(alloc_data), type=type)
 
 
+def _normalize_filename(filename):
+    if filename.startswith("src/"):
+        return filename[4:]
+    return filename
+
+
 @cli.command()
 @click.pass_obj
 @click.argument("filename")
 def annotate(obj, filename):
+    filename = _normalize_filename(filename)
+
     if obj.type == "total":
         alloc_str = lambda line: str(line["total_allocs"])
     else:
         alloc_str = lambda line: "{:.2f}".format(line["avg_allocs"])
 
-    if filename.startswith("src/"):
-        filename = filename[4:]
     filedata = obj.data[filename]
 
     linedata = {lineno: alloc_str(line) for lineno, line in filedata.items()}
@@ -53,6 +61,16 @@ def annotate(obj, filename):
         print(f"{linecount:>{maxlen}}  {line}", end="")
 
 
+def _list(obj, field, reverse):
+    file_sums = {
+        filename: sum(line[field] for line in lines.values())
+        for filename, lines in obj.data.items()
+    }
+
+    return sorted(
+        file_sums.items(), key=lambda x: x[1], reverse=reverse
+    )
+
 @cli.command()
 @click.pass_obj
 @click.option("-r", "--reverse", is_flag=True)
@@ -64,18 +82,55 @@ def list(obj, reverse):
         field = "avg_allocs"
         field_fmt = "{:.2f}"
 
-    file_sums = {
-        filename: sum(line[field] for line in lines.values())
-        for filename, lines in obj.data.items()
-    }
+    file_sums = _list(obj, field, reverse)
 
-    maxlen = max(len(field_fmt.format(l)) for l in file_sums.values())
-
-    for filename, file_sum in sorted(
-        file_sums.items(), key=lambda x: x[1], reverse=reverse
-    ):
+    maxlen = max(len(field_fmt.format(l[1])) for l in file_sums)
+    for filename, file_sum in file_sums:
         num_str = field_fmt.format(file_sum)
         print(f"{num_str:>{maxlen}}  {filename}")
+
+
+@cli.command()
+@click.pass_obj
+@click.option("-r", "--reverse", is_flag=True)
+@click.argument("htmldir")
+def html(obj, reverse, htmldir):
+    file_sums = _list(obj, "total_allocs", reverse)  #FIXME total
+
+    maxlen = 6
+    with open(f"{htmldir}/index.html", "w") as f:
+        f.write("<html><tt>")
+        for filename, file_sum in file_sums:
+            f.write(f"{file_sum:>{maxlen}}  <a href='{filename}.html'>{filename}</a><br>")
+        f.write("</tt></html>")
+
+    for filename in file_sums:
+        filename = _normalize_filename(filename[0])
+        htmlfile = Path(htmldir) / filename
+        htmlfile.parent.mkdir(parents=True, exist_ok=True)
+
+        with open(str(htmlfile) + ".html", "w") as f:
+            filedata = obj.data[filename]
+            f.write(f"<html><title>{filename}</title><tt><table>")
+            f.write(f"<tr>")
+            f.write(f"<td style='color: grey'><tt>#</tt></td>")
+            f.write("<td style='text-align: right'><tt>avg</tt></td>")
+            f.write("<td style='text-align: right'><tt>total</tt></td>")
+            f.write("<td></td>")
+            f.write("</tr>")
+            lineno = 0
+            for line in open(HERE / "src" / filename):
+                line = line.rstrip("\n").replace(" ", "&nbsp;")
+                lineno += 1
+                total = filedata.get(lineno, {}).get("total_allocs", 0)
+                avg = filedata.get(lineno, {}).get("avg_allocs", 0)
+                f.write("<tr>")
+                f.write(f"<td style='color: grey'><tt>{lineno}</tt></td>")
+                f.write(f"<td style='text-align: right'><tt>{avg:.2f}</tt></td>")
+                f.write(f"<td style='text-align: right'><tt>{total}</tt></td>")
+                f.write(f"<td><tt>{line}</tt></td>")
+                f.write("</tr>")
+            f.write("</table></tt></html>")
 
 
 if __name__ == "__main__":
--- core/prof/prof.py
+++ core/prof/prof.py
@@ -2,7 +2,7 @@ import sys
 
 from uio import open
 from uos import getenv
-import trezorutils
+import micropython
 
 # We need to insert "" to sys.path so that the frozen build can import main from the
 # frozen modules, and regular build can import it from current directory.
@@ -48,6 +48,14 @@ class _Prof:
     def coverage_data(self):
         return self.__coverage.lines_execution()
 
+    def write_data(self):
+        print("Total traces executed: ", __prof__.trace_count)
+        with open(".coverage", "w") as f:
+            # wtf so private much beautiful wow
+            f.write("!coverage.py: This is a private format, don't read it directly!")
+            # poormans json
+            f.write(str(__prof__.coverage_data()).replace("'", '"'))
+
 
 class AllocCounter:
     def __init__(self):
@@ -67,7 +75,7 @@ class AllocCounter:
         entry["calls"] += 1
 
     def trace_tick(self, frame, event, arg):
-        allocs_now = trezorutils.get_alloc_count()
+        allocs_now = micropython.alloc_count()
 
         if event != "line":
             return
@@ -75,32 +83,42 @@ class AllocCounter:
         allocs_per_last_line = allocs_now - self.last_alloc_count
         self.count_last_line(allocs_per_last_line)
         self.last_line = "{}:{}".format(frame.f_code.co_filename, frame.f_lineno)
-        self.last_alloc_count = trezorutils.get_alloc_count()
+        self.last_alloc_count = micropython.alloc_count()
 
     def dump_data(self, filename):
-        allocs_now = trezorutils.get_alloc_count()
+        allocs_now = micropython.alloc_count()
         allocs_per_last_line = allocs_now - self.last_alloc_count
         self.count_last_line(allocs_per_last_line)
         with open(filename, "w") as f:
             for key, val in self.data.items():
                 f.write("{} {total_allocs} {calls}\n".format(key, **val))
 
-
-ALLOC_COUNTER = AllocCounter()
+    def write_data(self):
+        self.dump_data("alloc_data.txt")
 
 
 def trace_handler(frame, event, arg):
-    ALLOC_COUNTER.trace_tick(frame, event, arg)
+    __prof__.trace_tick(frame, event, arg)
     return trace_handler
 
 
 def atexit():
     print("\n------------------ script exited ------------------")
-    ALLOC_COUNTER.dump_data("alloc_data.txt")
+    __prof__.write_data()
 
 
 sys.atexit(atexit)
 
+global __prof__
+if not "__prof__" in globals():
+    if getenv("TREZOR_MEMPERF") == "1":
+        __prof__ = AllocCounter()
+    else:
+        __prof__ = _Prof()
+
 sys.settrace(trace_handler)
-ALLOC_COUNTER.last_alloc_count = trezorutils.get_alloc_count()
+
+if isinstance(__prof__, AllocCounter):
+    __prof__.last_alloc_count = micropython.alloc_count()
+
 import main
